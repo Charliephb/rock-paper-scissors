bdef408ffc89a8003aa8338f89ada3e5a24fc1e8
Charlie Prod'homme-Bennett <charlieivor@icloud.com>
2023-01-16 15:32:46 +0000

    if-statements-for-point-adding

--- external.js
+++ external.js
@@ -1,11 +1,53 @@
 // create array of "weapons"
-const possibleSelections = ["Rock" , "Paper" , "Scissors"];
+const possibleSelections = ["rock" , "paper" , "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+let playerinput = prompt("Rock,Paper, Scissors?")
 
-// create function for random choice of move
-function getComputerChoice() {
-    computerSelection = possibleSelections[Math.floor(Math.random() * possibleSelections.length)];
-    return(computerSelection);
-}
 
 //create the if statements for the game 
+function playRound(playerSelection, computerSelection) {
+    // create function for random choice of move
+   function getComputerChoice() {
+    computerSelection = possibleSelections[Math.floor(Math.random() * possibleSelections.length)];
+    return(computerSelection);
+    }
 
+    //make both choices lower case for log
+    playerSelection = playerinput.toLowerCase();
+    getComputerChoice();
+
+    //tie
+    if (playerSelection == computerSelection) {
+    return(`This is a tie ${playerSelection} was chosen by both sides`) }
+    //player win
+    else if (
+        (playerSelection == 'rock' && computerSelection == 'scissors') || 
+        (playerSelection == 'paper' && computerSelection == 'rock') || 
+        (playerSelection == 'scissors' && computerSelection == 'paper') )  
+        {
+    // add to player score       
+    playerScore = ++playerScore;
+    return(`${playerSelection} beats ${computerSelection} - Player wins -> Player ${playerScore} - Computer ${computerScore}`) 
+    
+        }
+    //computer win
+    else if (
+        (playerSelection == 'rock' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'rock') )
+        {
+    // add to computer score
+    computerScore = ++computerScore;
+    return(`${computerSelection} beats ${playerSelection} - Computer wins -> Player ${playerScore} - Computer ${computerScore}`);
+    // return(`Player ${playerScore} - Computer ${computerScore}`);
+        }
+    
+     
+
+    
+
+
+    // else if (playerSelection == "Paper" && computerSelection == "Scissors")
+    
+    }

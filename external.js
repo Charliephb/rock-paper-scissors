07a1c9d879a67f6db224a4db7c3f26ffa2822022
Charlie Prod'homme-Bennett <charlieivor@icloud.com>
2023-01-16 14:03:21 +0000

    created-array-and-random-function

--- external.js
+++ external.js
@@ -1,9 +1,11 @@
 // create array of "weapons"
-const possibleSelections = ("Rock" , "Paper" , "Scissors");
+const possibleSelections = ["Rock" , "Paper" , "Scissors"];
 
-
-
-
+// create function for random choice of move
 function getComputerChoice() {
-
+    computerSelection = possibleSelections[Math.floor(Math.random() * possibleSelections.length)];
+    return(computerSelection);
 }
+
+//create the if statements for the game 
+
